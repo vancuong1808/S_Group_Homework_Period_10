a0d9958e3d3479a88fa715d6fcee960ca58a3b6c
Cường Văn <vancuong1808@gmail.com>
2024-03-06 16:25:41 +0700

    first init

---
+++ main.js
@@ -0,0 +1,38 @@
+let username = document.getElementById("user_name");
+let password = document.getElementById("pass_word");
+let logIn = document.querySelector('.button');
+let Isusername = false;
+let Ispassword = false;
+
+username.addEventListener('input', function() {
+    // console.log("alo", username.value);
+    // console.log( username.value[0] )
+    if ( !username.checkValidity() ) {
+        document.querySelector('.Invalid_username').style.display = 'block';
+        Isusername = false;
+    } else {
+        document.querySelector('.Invalid_username').style.display = 'none';
+        Isusername = true;
+    }
+    console.log( Isusername );
+})
+password.addEventListener('input', function() {
+    // console.log("alo", username.value);
+    if ( !password.checkValidity() ) {
+    document.querySelector('.Invalid_password').style.display = 'block';
+    Ispassword = false;
+    } else {
+    document.querySelector('.Invalid_password').style.display = 'none';
+    Ispassword = true;
+    }
+    console.log( Ispassword );
+})
+// console.log( Isusername );
+// console.log( Ispassword );
+logIn.addEventListener( 'onclick', function() {
+    if ( Isusername == true && Ispassword == true ) {
+        alert("Success Login ");
+    } else {
+        alert("Fail Login");
+    }
+})
